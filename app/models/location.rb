d43f338c6e27a704b071b23823d0fabfea8e1f74
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-05 16:18:47 -0400

    hound line corrections

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,18 +2,20 @@ class Location < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   validates :state, presence: true
-  validates :state, inclusion: { in: ["AL",
-    "AK", "AZ", "AR", "CA",
-    "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL",
-    "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
-    "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI","SC", "SD", "TN", "TX", "UT", "VT", "VA",
-    "WA", "WV", "WI", "WY", "GU", "PR", "VI"]}
+  validates :state, inclusion: { in: [
+    "AL","AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
+    "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
+    "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
+    "OR", "PA", "RI","SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
+    "WI", "WY", "GU", "PR", "VI"
+    ]}
   validates :description, presence: true
 
-  STATES = ["AL",
-    "AK", "AZ", "AR", "CA",
-    "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL",
-    "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
-    "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI","SC", "SD", "TN", "TX", "UT", "VT", "VA",
-    "WA", "WV", "WI", "WY", "GU", "PR", "VI"]
+  STATES = [
+    "AL","AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
+    "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
+    "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
+    "OR", "PA", "RI","SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
+    "WI", "WY", "GU", "PR", "VI"
+    ]
 end
