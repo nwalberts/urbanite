b406194e57d5fdd3ce6e356917788f0d91a0a8f5
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-05 14:12:10 -0400

    took out another line violations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,13 +4,11 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 
-
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
 
   config.use_transactional_fixtures = true
 
@@ -18,7 +16,5 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
-
   config.filter_rails_from_backtrace!
-
 end
