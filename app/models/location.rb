b2564a1e62a38c5c7b5e199be7782ce8512e4c37
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-05 16:23:56 -0400

    hound line corrections 3

--- app/models/location.rb
+++ app/models/location.rb
@@ -17,7 +17,7 @@ class Location < ActiveRecord::Base
   "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
   "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
   "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
-  "OR", "PA", "RI","SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
+  "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
   "WI", "WY", "GU", "PR", "VI"
   ]
 end
