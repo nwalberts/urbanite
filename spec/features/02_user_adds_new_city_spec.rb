576b92a969146f2a9f02b216f74d411763ab43b7
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-05 14:02:00 -0400

    edited for line length

--- spec/features/02_user_adds_new_city_spec.rb
+++ spec/features/02_user_adds_new_city_spec.rb
@@ -1,10 +1,5 @@
 require "rails_helper"
 
-# [] A city must have a name, state, and description.
-# [] Visiting the `/locations/new` path should display a form for creating a new city.
-# [] When adding a new city, if I fill out the form correctly, I should see the page for the newly created city.
-# [] When adding a new city, if I fill out the form incorrectly and submit the form, I should see the form and be displayed the validation errors.
-
 feature "visitors can add cities" do
   scenario "visitor adds new city successfully" do
 
@@ -26,6 +21,7 @@ feature "visitors can add cities" do
     visit new_city_path
 
     click_button "Add City"
-    expect(page).to have_content "Name can't be blank. State can't be blank. Description can't be blank."
+    expect(page).to have_content %q(Name can't be blank. State can't be blank.
+    Description can't be blank.)
   end
 end
