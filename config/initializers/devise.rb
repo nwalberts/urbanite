a8fd8b1243bf7054f065fbb5cf11cb1312ad485c
Mia Gaulin <gaulin.mia@gmail.com>
2016-04-05 16:09:50 -0400

    hound updates

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.mailer_sender =
-  'please-change-me-at-config-initializers-devise@example.com'
+    'please-change-me-at-config-initializers-devise@example.com'
 
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
