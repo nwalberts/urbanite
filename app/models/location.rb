7cf65f3e4d02cd5725eb4753a83220a703b161d6
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-06 11:46:59 -0400

    remove duplicates from origin merg

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,4 @@
 class Location < ActiveRecord::Base
-  validates :name, presence: true
-  validates :name, uniqueness: { case_sensitive: false }
-  validates :state, presence: true
-  validates :state, inclusion: { in: STATES }
-  validates :description, presence: true
-
   STATES =
     [
       "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
@@ -12,5 +6,12 @@ class Location < ActiveRecord::Base
       "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
       "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
       "WI", "WY", "GU", "PR", "VI"
-    ].freeze
+    ]
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :state, presence: true
+  validates :state, inclusion: { in: STATES }
+  validates :description, presence: true
+
+
 end
