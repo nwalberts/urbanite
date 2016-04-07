3202b1cd5a1ea4cca5a44192d68c04da56588ed9
Mia Gaulin <gaulin.mia@gmail.com>
2016-04-07 14:41:24 -0400

    hound updates

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Review do
   it { should belong_to :user }
   it { should belong_to :location }
-  
+
   it { should have_valid(:rating).when("5", "1") }
   it { should_not have_valid(:rating).when(nil, "", "6") }
 
