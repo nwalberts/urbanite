ddaf45fb64878d93e7f7155c92bb1a781b5758f5
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-08 14:27:11 -0400

    add FactoryGirl for reviews

--- spec/features/user_views_city_details_spec.rb
+++ spec/features/user_views_city_details_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 feature "visitor sees show page for a city" do
   scenario "clicks link and is taken to show page for given city" do
-    boston = FactoryGirl.create(:location)
+    review = FactoryGirl.create(:review)
     visit locations_path
 
     click_link "Boston"
 
-    expect(page).to have_content boston.name
-    expect(page).to have_content boston.state
-    expect(page).to have_content boston.description
+    expect(page).to have_content review.location.name
+    expect(page).to have_content review.location.state
+    expect(page).to have_content review.location.description
+    expect(page).to have_content review.body
+    expect(page).to have_content review.rating
   end
 end
