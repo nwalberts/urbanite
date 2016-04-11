d150cbf558b5437fff5749f33a5ff458b05d3c99
Mia Gaulin <gaulin.mia@gmail.com>
2016-04-11 17:41:49 -0400

    fixed sign in redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "locations#index"
 
   resources :profiles, only: [:index, :new, :create, :show, :edit, :update]
-  resources :locations
+  # resources :locations
 
   resources :locations do
     resources :reviews, only: [:new, :create, :show, :edit, :update]
@@ -14,6 +14,5 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "users/sign_out" => "devise/sessions#destroy"
-    post "users/sign_in" => "devise/sessions#create"
   end
 end
