c2348eab4266695745b1fa0236847878b3a66db6
Britta <brittacarroll@gmail.com>
2016-04-11 14:15:35 -0400

    hound violations

--- spec/features/user_views_city_details_spec.rb
+++ spec/features/user_views_city_details_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 feature "visitor sees show page for a city" do
   let!(:admin) { User.create(first_name: "Greg", last_name: "Ward", home_location: "Boston", email: "greg@la.com", password: "password1", role: "admin") }
+  FactoryGirl.create(:location)
 
   scenario "clicks link and is taken to show page for given city" do
-    FactoryGirl.create(:location)
-
     visit 'users/sign_in'
     fill_in "Email", with: admin.email
     fill_in "Password", with: admin.password
