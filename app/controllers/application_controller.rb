f85fceb8f7b8bd5410e2f6a2b43ac3ffb3065486
Britta <brittacarroll@gmail.com>
2016-04-11 14:22:19 -0400

    deleted commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #   request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-  # end
-
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  # , controllers: { registrations: "users/registrations" }
 
   devise_scope :user do
     get "users/sign_out" => "devise/sessions#destroy"
