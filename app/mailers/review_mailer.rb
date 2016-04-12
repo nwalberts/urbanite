ad1d4a73bb07ba6e7e9f86e69ddfa78ee4a531fc
Gregory Ward <wardg27@gmail.com>
2016-04-12 13:38:00 -0400

    initial email functionality

---
+++ app/mailers/review_mailer.rb
@@ -0,0 +1,10 @@
+class ReviewMailer < ApplicationMailer
+  def new_review(review)
+    @review = review
+
+    mail(
+      to: review.location.user.email
+      subject: "New Review for #{review.location.name}"
+    )
+  end
+end
---
+++ spec/features/user_recieves_email_after_review_spec.rb
@@ -0,0 +1,26 @@
+require 'rail_helper'
+
+feature "User recieves an email after their location has been reviewed" do
+  let!(:review) { FactoryGirl.create(:review) }
+  scenario "user reviews a location" do
+    
+    visit 'users/sign_in'
+    fill_in "Email", with: review.user.email
+    fill_in "Password", with: review.user.password
+    click_button "Log in"
+
+    visit "/locations"
+
+    click_link review.location.name
+
+    click_link "Add a Review"
+    choose('review_rating_5')
+    fill_in "Body", with: "Beautiful!"
+    click_button "Submit"
+
+    expect(page).to have_content("Review added successfully!")
+    expect(page).to have_content("5/5")
+    expect(page).to have_content("Beautiful!")
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
+end
