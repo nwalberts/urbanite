e200af101e2f6a305c38fa02ac119facd8de72f9
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-14 16:56:12 -0400

    hound 3

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,3 +1,51 @@
+var writeUpvote = function(id) {
+  var reviewIdSelector = "#review-" + id;
+  var currentVote = $(reviewIdSelector).text();
+  if (currentVote == 1) {
+  $(reviewIdSelector).text(parseInt(currentVote) - 1);
+} else if (currentVote == -1) {
+    $(reviewIdSelector).text(parseInt(currentVote) + 2);
+} else {
+  $(reviewIdSelector).text(parseInt(currentVote) + 1);
+}
+};
+
+var updateUpvote = function(url) {
+  var reviewId = window.location.pathname.match(/\/(\d+)$/)[1];
+  $.ajax({
+    method: "POST",
+    url: url,
+    data: { review_id: reviewId },
+    success: function(responseData) {
+      writeUpvote(responseData.id);
+    },
+  });
+};
+
+var writeDownvote = function(id) {
+  var reviewIdSelector = "#review-" + id;
+  var currentVote = $(reviewIdSelector).text();
+  if (currentVote == -1) {
+  $(reviewIdSelector).text(parseInt(currentVote) + 1);
+} else if (currentVote == 1) {
+    $(reviewIdSelector).text(parseInt(currentVote) - 2);
+} else {
+  $(reviewIdSelector).text(parseInt(currentVote) - 1);
+}
+};
+
+var updateDownvote = function(url) {
+  var reviewId = window.location.pathname.match(/\/(\d+)$/)[1];
+  $.ajax({
+    method: "POST",
+    url: url,
+    data: { review_id: reviewId },
+    success: function(responseData) {
+      writeDownvote(responseData.id);
+    },
+  });
+};
+
 $(document).ready(function() {
   $(".new_upvote").on("click", function(event) {
     event.preventDefault();
@@ -13,57 +61,3 @@ $(document).ready(function() {
     updateDownvote(url);
   });
 });
-
-var updateUpvote = function(url) {
-  var reviewId = window.location.pathname.match(/\/(\d+)$/)[1];
-  $.ajax({
-    method: "POST",
-    url: url,
-    data: { review_id: reviewId },
-    success: function(responseData) {
-      writeUpvote(responseData.id);
-    },
-    error: function()  {
-      alert("error!");
-    }
-  });
-};
-
-var writeUpvote = function(id) {
-  var reviewIdSelector = '#review-' + id;
-  var currentVote = $(reviewIdSelector).text();
-  if (currentVote == 1) {
-  $(reviewIdSelector).text(parseInt(currentVote) - 1);
-} else if (currentVote == -1) {
-    $(reviewIdSelector).text(parseInt(currentVote) + 2);
-} else {
-  $(reviewIdSelector).text(parseInt(currentVote) + 1);
-}
-};
-
-var updateDownvote = function(url) {
-  var reviewId = window.location.pathname.match(/\/(\d+)$/)[1];
-  $.ajax({
-    method: "POST",
-    url: url,
-    data: { review_id: reviewId },
-    success: function(responseData) {
-      writeDownvote(responseData.id);
-    },
-    error: function()  {
-      alert("error!");
-    }
-  });
-};
-
-var writeDownvote = function(id) {
-  var reviewIdSelector = "#review-" + id;
-  var currentVote = $(reviewIdSelector).text();
-  if (currentVote == -1) {
-  $(reviewIdSelector).text(parseInt(currentVote) + 1);
-} else if (currentVote == 1) {
-    $(reviewIdSelector).text(parseInt(currentVote) - 2);
-} else {
-  $(reviewIdSelector).text(parseInt(currentVote) - 1);
-}
-};
